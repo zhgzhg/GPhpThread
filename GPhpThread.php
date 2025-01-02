54f49ab92344b2ec432c1bcedc82915eec066164
zhgzhg <zhgzhg@users.noreply.github.com>
2025-01-02 22:14:54 +0200

    Update copyright

--- GPhpThread.php
+++ GPhpThread.php
@@ -2,7 +2,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2024 zhgzhg
+ * Copyright (c) 2025 zhgzhg
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
